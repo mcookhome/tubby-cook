5be839b6fcd5f4c25f92747037bb6edcf2def690
Matthew Cook <mcookhome@gmail.com>
2014-11-11 20:36:33 -0500

    added spotify

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,url_for,redirect,render_template,session,request
-import urllib2, json
+import urllib2, json,unicodedata
 import oauth2
 
 app=Flask(__name__)
@@ -14,7 +14,7 @@ TOKEN_SECRET='LTJDS6mS7yr4EfVWYL4LiwhzIXI'
 def req(url_params=None):
     url_params = url_params or {}
     url='http://{0}{1}?'.format('api.yelp.com','/v2/search/')
-    
+   
     consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
     oauth_request = oauth2.Request(method="GET",url=url, parameters=url_params)
     oauth_request.update(
@@ -25,9 +25,11 @@ def req(url_params=None):
             'oauth_consumer_key': CONSUMER_KEY
         }
     )
+
     token = oauth2.Token(TOKEN, TOKEN_SECRET)
     oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
     signed_url=oauth_request.to_url()
+    print signed_url
     print 'Querying {0} ...".format(url)'
     conn=urllib2.urlopen(signed_url, None)
     response = json.loads(conn.read())
@@ -44,16 +46,56 @@ def t():
         tag= request.form['tag']
         location=request.form['loc']
         url_params ={'term':tag.replace(" ", "+"),'location':location.replace(" ", "+"), 'limit': 5, 'category_filter': 'food'}
+
         page = ""
         for x in req(url_params)['businesses']:
             print x[u'name']+" - "+x[u'snippet_text'].replace("\n"," ")
-            page = x[u'name']+" - "+x[u'snippet_text'].replace("\n"," ") + "<br>"
+            page += x[u'name']+" - "+x[u'snippet_text'].replace("\n"," ") + "<br>"
         return page
     else:
         return render_template("base.html")
 
-
-
+@app.route("/spot", methods=['GET', 'POST'])
+def spot():
+    stri="Dianda's Italian American Pastry"
+    L=stri.split()
+    L[:] = [ urlify(o) for o in L]
+    print L
+    k = 20/len(L)-1
+    i=0
+    songids=[]
+    for n in L:
+        print n
+        conn=urllib2.urlopen(n, None)
+        response = json.loads(conn.read())
+        conn.close()
+        ppp= response['tracks']['items']
+        for m in ppp:
+            song = m['id']
+            print song
+            songids.append(song)
+            print "i: "+str(i)
+            print "k: "+str(k)
+            if i == k:
+                print"broken"
+                i=0
+                break
+            else:
+                i = i+1
+    print songids
+    songids[:]=[unicodedata.normalize('NFKD',o).encode('ascii','ignore') for o in songids]
+    print songids
+    tracklist=""
+    for n in songids:
+        tracklist += n + ","
+    tracklist = tracklist[:-1]
+    print "dingdong"+tracklist
+        
+    return render_template("spotify.html", s=tracklist)
+    
+def urlify(s):
+    url = "https://api.spotify.com/v1/search?q=" +s+ "&type=track"
+    return url
 
 if __name__=="__main__":
    app.debug=True
